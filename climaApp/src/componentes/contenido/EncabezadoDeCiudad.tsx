023a8148cdf1762cfe119e28878e40e86225bd7b
Rocio albarracin <ralbarracintrab@gmail.com>
2026-05-01 22:07:29 -0300

    en el anterior commit me olvide mencionar que arregle el problema con el boton para cambiar de tema, ahora esta solucionado. Y agregue estilos para título

--- climaApp/src/componentes/contenido/EncabezadoDeCiudad.tsx
+++ climaApp/src/componentes/contenido/EncabezadoDeCiudad.tsx
@@ -1,11 +1,20 @@
-import type { ClimaPorDia } from '../../tipos/infoClima';
 import { Text } from 'react-native';
+import { useColorScheme } from 'nativewind';
 
 const EncabezadoDeCiudad = ({ ciudad }: { ciudad: string | null }) => {
+  const { colorScheme } = useColorScheme();
+
   return (
-    <Text>
+    <Text style={{
+      fontSize: 22,
+      fontWeight: '500',
+      textAlign: 'center',
+      color: colorScheme === 'dark' ? '#ffffff' : '#000000',
+      opacity: 0.85,
+    }}>
       {ciudad}
     </Text>
   );
 };
+
 export default EncabezadoDeCiudad;
